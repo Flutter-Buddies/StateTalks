c298b2b1ece9b8931de827b6015bd5942a65a4bf
Sandro Lovnički <sandro@scipack.xyz>
2020-11-29 17:05:06 +0100

    provider/example/: [add] basic Stateful

--- provider/example/lib/main.dart
+++ provider/example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  int _counterValue = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,49 +32,75 @@ class MyHomePage extends StatelessWidget {
       body: Stack(
         alignment: Alignment.center,
         children: <Widget>[
-          CounterValue(),
-          ControlButtons(),
+          Text(
+            '$_counterValue',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(48.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () => setState(() => _counterValue++),
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => setState(
+                        () => _counterValue = math.Random().nextInt(20)),
+                    child: Text('?', style: TextStyle(fontSize: 18)),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () => setState(() => _counterValue--),
+                    child: Icon(Icons.remove),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class CounterValue extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      '0',
-      style: Theme.of(context).textTheme.headline1,
-    );
-  }
-}
+// class CounterValue extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       '0',
+//       style: Theme.of(context).textTheme.headline1,
+//     );
+//   }
+// }
 
-class ControlButtons extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Padding(
-        padding: const EdgeInsets.all(48.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Text('?', style: TextStyle(fontSize: 18)),
-            ),
-            FloatingActionButton(
-              onPressed: () {},
-              child: Icon(Icons.remove),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class ControlButtons extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Align(
+//       alignment: Alignment.bottomCenter,
+//       child: Padding(
+//         padding: const EdgeInsets.all(48.0),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             FloatingActionButton(
+//               onPressed: () {},
+//               child: Icon(Icons.add),
+//             ),
+//             FloatingActionButton(
+//               onPressed: () {},
+//               child: Text('?', style: TextStyle(fontSize: 18)),
+//             ),
+//             FloatingActionButton(
+//               onPressed: () {},
+//               child: Icon(Icons.remove),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
