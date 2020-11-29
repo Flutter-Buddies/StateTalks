9b142cfa79f5f4fc1351b352d3bdffa895916071
Sandro Lovnički <sandro@scipack.xyz>
2020-11-29 16:58:16 +0100

    provider/: [add] Stateless app

---
+++ provider/example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Talks: provider'),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          CounterValue(),
+          ControlButtons(),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterValue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '0',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
+
+class ControlButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(48.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Text('?', style: TextStyle(fontSize: 18)),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.remove),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
