ca795ba47440f5ef7a51f9215d838b1ea9871a55
Sandro Lovnički <sandro@scipack.xyz>
2020-12-06 14:31:11 +0100

    my_provider/example/: [implement] MyProvider

---
+++ my_provider/example/lib/my_inherited_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class _MyInheritedWidget<T extends ChangeNotifier> extends InheritedWidget {
+  _MyInheritedWidget({Key key, Widget child, _MyProviderState<T> state})
+      : state = state,
+        super(key: key, child: child);
+
+  final _MyProviderState<T> state;
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+  static _MyInheritedWidget<T> of<T extends ChangeNotifier>(
+      BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<_MyInheritedWidget<T>>();
+  }
+}
+
+class MyProvider<T extends ChangeNotifier> extends StatefulWidget {
+  final T data;
+  final Widget child;
+
+  MyProvider({this.data, this.child});
+
+  @override
+  _MyProviderState<T> createState() => _MyProviderState<T>();
+
+  static _MyProviderState<T> of<T extends ChangeNotifier>(
+      BuildContext context) {
+    return _MyInheritedWidget.of<T>(context).state;
+  }
+}
+
+class _MyProviderState<T extends ChangeNotifier> extends State<MyProvider<T>> {
+  T data;
+
+  @override
+  void initState() {
+    data = widget.data;
+    data.addListener(() => setState(() => {}));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _MyInheritedWidget<T>(
+      state: this,
+      child: widget.child,
+    );
+  }
+}
+
+extension ExtractData on BuildContext {
+  T extract<T extends ChangeNotifier>() => MyProvider.of<T>(this).data;
+}
